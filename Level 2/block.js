dc95c28fbaab1dc6b836fb931b8deaeacc1de0b7
Piyush <piyushkumar2738@gmail.com>
2022-08-22 10:46:22 +0530

    New edits made.

---
+++ Level 2/block.js
@@ -0,0 +1,27 @@
+// {
+//     console.log("Hello")
+// }
+// console.log("world");
+
+
+// let score = 50;
+
+// if (score < 90) {
+//     var pass1 = false;
+//     let pass2 = false;
+//     const pass3 = false;
+// }
+
+// console.log(pass1); // False
+// console.log(pass2); // Error 
+// console.log(pass3); // Error
+
+
+
+
+console.log(a) // Undefined;
+console.log(b) // cannot access
+console.log(c) // cannot access
+var a = 10;
+let b = 20;
+const c = 30;
